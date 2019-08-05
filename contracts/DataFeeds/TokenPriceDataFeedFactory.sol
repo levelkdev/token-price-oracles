f0fdd529cf1e1ceed31ab0f1617f62a4fc23d96d
Mike Calvanese <mfcalvanese@gmail.com>
2019-08-05 12:31:12 -0400

    add TokenPriceDataFeedFactory

---
+++ contracts/DataFeeds/TokenPriceDataFeedFactory.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.4.24;
+
+import "./TokenPriceDataFeed.sol";
+
+contract TokenPriceDataFeedFactory {
+
+  event TokenPriceDataFeedCreated(address tokenPriceDataFeed, address token1, address token2);
+
+  IExchangeAdapter public exchangeAdapter;
+
+  constructor(IExchangeAdapter _exchangeAdapter) public {
+    exchangeAdapter = _exchangeAdapter;
+  }
+
+  function createTokenPriceDataFeed(
+    address token1,
+    address token2
+  ) public {
+    TokenPriceDataFeed tokenPriceDataFeed = new TokenPriceDataFeed();
+    tokenPriceDataFeed.initialize(token1, token2, exchangeAdapter);
+
+    // TODO: require that token1 and token2 exchanges exist
+
+    emit TokenPriceDataFeedCreated(address(tokenPriceDataFeed), token1, token2);
+  }
+
+}
---
+++ test/TokenPriceDataFeedFactory.test.js
@@ -0,0 +1,48 @@
+const TokenPriceDataFeedFactory = artifacts.require('TokenPriceDataFeedFactory.sol')
+const TokenPriceDataFeed = artifacts.require('TokenPriceDataFeed.sol')
+const UniswapAdapterMock = artifacts.require('UniswapAdapterMock.sol')
+
+const token1 = '0x19a150a4e966bac5bd0473153f5c526d8fa7d4e7'
+const token2 = '0xa27d79db9c02ab01de0ca0f8468cbad2c578f609'
+
+contract('TokenPriceDataFeedFactory', (accounts) => {
+  let tokenPriceDataFeedFactory, uniswapAdapterMock
+
+  beforeEach(async () => {
+    uniswapAdapterMock = await UniswapAdapterMock.new()
+    tokenPriceDataFeedFactory = await TokenPriceDataFeedFactory.new(uniswapAdapterMock.address)
+  })
+
+  it('should set the correct exchangeAdapter', async () => {
+    expect(
+      await tokenPriceDataFeedFactory.exchangeAdapter()
+    ).to.equal(uniswapAdapterMock.address)
+  })
+
+  describe('createTokenPriceDataFeed()', () => {
+
+    describe('when given valid parameters', () => {
+
+      let tokenPriceDataFeed
+
+      beforeEach(async () => {
+        const { logs } = await tokenPriceDataFeedFactory.createTokenPriceDataFeed(
+          token1,
+          token2
+        )
+        tokenPriceDataFeed = await TokenPriceDataFeed.at(logs[0].args.tokenPriceDataFeed)
+      })
+
+      it('should create a TokenPriceDataFeed with the correct token pair', async () => {
+        expect(await tokenPriceDataFeed.token1()).to.equal(token1)
+        expect(await tokenPriceDataFeed.token2()).to.equal(token2)
+      })
+
+      it('should create a TokenPriceDataFeed with the correct exchange adapter', async () => {
+        expect(await tokenPriceDataFeed.exchangeAdapter()).to.equal(uniswapAdapterMock.address)
+      })
+
+    })
+  })
+
+})
