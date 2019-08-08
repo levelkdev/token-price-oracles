920a68f9a7757f541844284b5721d6563fb1ab6e
Mike Calvanese <mfcalvanese@gmail.com>
2019-08-08 13:48:18 -0500

    don't allow the TokenPriceDataFeedFactory to create duplicates
    and add some view functions to suppor this check

--- contracts/DataFeeds/TokenPriceDataFeedFactory.sol
+++ contracts/DataFeeds/TokenPriceDataFeedFactory.sol
@@ -8,6 +8,9 @@ contract TokenPriceDataFeedFactory {
 
   IExchangeAdapter public exchangeAdapter;
 
+  // token1 => token2 => TokenPriceDataFeed
+  mapping(address => mapping(address => TokenPriceDataFeed)) public tokenPairToDataFeed;
+
   constructor(IExchangeAdapter _exchangeAdapter) public {
     exchangeAdapter = _exchangeAdapter;
   }
@@ -16,10 +19,30 @@ contract TokenPriceDataFeedFactory {
     address token1,
     address token2
   ) public {
+    require(!tokenPriceDataFeedExists(token1, token2));
+
     TokenPriceDataFeed tokenPriceDataFeed = new TokenPriceDataFeed();
     tokenPriceDataFeed.initialize(token1, token2, exchangeAdapter);
 
+    tokenPairToDataFeed[token1][token2] = tokenPriceDataFeed;
+
     emit TokenPriceDataFeedCreated(address(tokenPriceDataFeed), token1, token2);
+  }
+
+  function getTokenPriceDataFeed(address token1, address token2)
+    public
+    view
+    returns (TokenPriceDataFeed)
+  {
+    return tokenPairToDataFeed[token1][token2];
+  }
+
+  function tokenPriceDataFeedExists(address token1, address token2)
+    public
+    view
+    returns (bool)
+  {
+    return address(getTokenPriceDataFeed(token1, token2)) != address(0);
   }
 
 }
