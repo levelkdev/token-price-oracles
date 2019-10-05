7af12ff91b2590de23680f29ca4d9867ec672562
Mike Calvanese <mfcalvanese@gmail.com>
2019-10-04 22:51:03 -0700

    add mock price setting function for ExchangeAdapterMock

--- test/mocks/ExchangeAdapterMock.sol
+++ test/mocks/ExchangeAdapterMock.sol
@@ -5,6 +5,7 @@ import "../../contracts/ExchangeAdapters/IExchangeAdapter.sol";
 contract ExchangeAdapterMock is IExchangeAdapter {
 
   mapping(address => bool) mock_tokenExists;
+  mapping(address => mapping(address => uint)) mock_tokenPrices;
 
   constructor(address[] tokenAddresses) {
     for(uint i = 0; i < tokenAddresses.length; i++) {
@@ -12,8 +13,12 @@ contract ExchangeAdapterMock is IExchangeAdapter {
     }
   }
 
+  function setPriceForTokenPair(address token1, address token2, uint price) {
+    mock_tokenPrices[token1][token2] = price;
+  }
+
   function getPriceForTokenPair(address token1, address token2) public view returns (uint price) {
-    return 1;
+    return mock_tokenPrices[token1][token2];
   }
 
   function tokenPairExists(address token1, address token2) public view returns (bool) {
